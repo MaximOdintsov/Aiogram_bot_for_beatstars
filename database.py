257c6b20c13dfb229780cb992df6f19868331904
Maxim <flipsidebeat@gmail.com>
2022-06-12 21:52:36 +0300

    Test_1

---
+++ database.py
@@ -0,0 +1,15 @@
+username = 'flipsidebeats'
+sign = 'DminornoMusicStArs56'
+
+TOKEN = '5478151046:AAFqMhNrppTeUD2fG34dIiPE-uV0TsiYvfY'
+
+
+
+usernam = 'macsim05.macsim@mail.ru'
+passwor = 'Vevahu51'
+
+
+username1 = 'flipsud'
+password1 = 'vevahu51'
+
+
