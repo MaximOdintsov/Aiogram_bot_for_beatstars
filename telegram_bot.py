47a1e8787dfc4152b8a9fb233c21e31a2b54c03a
Maxim <flipsidebeat@gmail.com>
2022-06-21 12:04:14 +0300

    Test_4_8.2

--- telegram_bot.py
+++ telegram_bot.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='1.1.1.1', port=port)
 
 
 class BeatstarsBot:
